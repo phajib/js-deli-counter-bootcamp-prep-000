f617b8c504dc00df181949928c409b6e7d3614a0
H B <phajib@gmail.com>
2018-11-30 18:54:21 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,25 @@
+function takeANumber(array, person){
+  array.push(person)
+  return `Welcome, ${person}. You are number ${array.length} in line.`
+
+  if (array.length === 0){
+    array.push(person)
+    return `Welcome, ${person}. You are numner ${array.length} in line. `
+  }return array
+}
+
+function nowServing(array){
+  var i = 0
+  if(array.length === 0){
+    return `There is nobody waiting to be served!`
+  }else{
+  return `Currently serving ${array[i]}.`
+  //var varray = array
+  array.shift()
+  return array
+  }
+}
+
+function currentLine(line){
+
+}
